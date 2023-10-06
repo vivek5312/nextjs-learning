5084bd9ad7f590ed1cc996b565f6f0ea61fa250b
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-10-06 09:37:57 +0530

    font optimization

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { Roboto } from "next/font/google";
+
+const roboto=Roboto({
+    weight:'300',
+    subsets:['latin']
+})
 
 const News =()=>{
 
     return (
     
     <div>
-        <h1>My News Page</h1>
+        <h1 className={roboto.className}>My News Page</h1>
         <Link href="/">Go to home page</Link>
         </div>
     )
